bbec48923e6fae9f6574773059972782c32306ef
Olga_Sanni <dev.sannikov@gmail.com>
2020-03-11 16:21:36 -0400

    First commit

---
+++ code.js
@@ -0,0 +1,22 @@
+class Inventions{
+    constructor(name, information){
+        this.name=name;
+        this.information=information;
+    }
+}
+
+const wheel=new Inventions("Wheel","The wheel stands out as the OG of engineering marvels and one of the most famous inventions that influenced numerous other things. This primitive technology made it easier for all of us to travel. From the archeological excavations, the oldest known wheel is from Mesopotamia, around 3500 B.C. As a result of advancement in the new and innovative design of wheels, industrialization could take root.The wheel serves a vital purpose in our lives, and we couldn't imagine the world without them.");
+const compass=new Inventions("Compass", "Created for spiritual and navigational purposes, the earliest compasses were most likely invented by the Chinese in around 1050 BC. It was made of lodestones, which is a naturally magnetized iron ore.The invention of the electromagnet in 1825 led to the development of the modern compass.The invention of the compass certainly helped modern navigation more than our GPS-needing culture could understand.");
+const automobile=new Inventions
+const steamEngine=new Inventions
+const petrol=new Inventions
+const antibiotics=new Inventions
+const railways=new Inventions
+const nail=new Inventions
+const tools=new Inventions
+const lightBulb=new Inventions
+const electricity=new Inventions
+const printingPress=new Inventions
+const transistors=new Inventions
+const computer=new Inventions
+const steel=new Inventions
